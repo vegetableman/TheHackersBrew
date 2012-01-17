cf1aeb63ee01e25045f0c6d569361968ba4d7150
Vignesh Anand <vigneshanand17@gmail.com>
2012-01-18 05:11:43 +0530

    Added comments and modified handle equation

--- js/booze.js
+++ js/booze.js
@@ -1,6 +1,7 @@
 
 function booze(domid,width,height,ingredients){
-    
+
+//Resig style : check for instantiation    
   if(!(this instanceof arguments.callee)){
     return new arguments.callee(arguments);
   }
@@ -20,24 +21,31 @@ function booze(domid,width,height,ingredients){
     self.boozeFonts = ['Courier New','Arial','Lucida Console'];
   }
 
+
+//fill Bottom part of the jug
   self.fillBottom = function(){
-    var hypX=250, hypY=100,rows=5;
+    var hypX=250, hypY=100;
+   //No. of rows at the bottom
+    var rows=5;
     var traverseFlag=1,ingredientsSlice=[];
     for(var row=1;row<rows;row++){
       hypX -= row*20;
       hypY -= 20;
+   //flag to traverse from left to right and right to left
       traverseFlag *= -1;
+   //Decrease the no. of elements to loop over on increase of row counter
       ingredientsSlice = $(ingredients).slice(0,$(ingredients).size()/row);
       self.fillBottomHalf(ingredientsSlice,self.boozeFonts,hypX,hypY,traverseFlag);
   }
 }
-  
+
   self.fillBottomHalf = function(ingredients,boozeFonts,hypX,hypY,traverseFlag){
     var bottomLabel,bottomColors=["#2C3033","#000000"];
     var angle = (Math.PI)/($(ingredients).size()-1);
     var dx = 0,dy = 0,interval=100;
     console.log('<<<<<Size:'+$(ingredients).size()+'>>>>>');
     $.each(ingredients,function(index,content){
+    //multiply interval with index so as to see the delay in traverse 
       $(this).delay(interval*index).queue(function(){
         console.log('<<<<<'+content.ingredient+'>>>>>')
         dx = traverseFlag*hypX*Math.cos(angle*index*-1);
@@ -54,11 +62,14 @@ function booze(domid,width,height,ingredients){
         bottomLabel = self.paper.text()
         bottomLabel.attr(attrObj);
         bottomLabel.translate(600,600);
+     //empty the queue for the new rows to show up
         $(this).dequeue();
       });
     });
   }
 
+//fill text contents in the middle portion
+//Need to simplify this function
   self.fillMid = function(){
     var startY=580,startX,endX,midLabel,bottomColors=["#2C3033","#000000"];
     var dx=0;
@@ -119,9 +130,10 @@ function booze(domid,width,height,ingredients){
     }
   }
 
-//Bezier Cubic Curve transformation
-//Reference:-http://processingjs.nihongoresources.com/bezierinfo/
-//Reference:-http://www.paultondeur.com/2008/03/09/drawing-a-cubic-bezier-curve-using-actionscript-3/
+/*Bezier Cubic Curve transformation
+ Reference:-http://processingjs.nihongoresources.com/bezierinfo/
+ Reference:-http://www.paultondeur.com/2008/03/09/drawing-a-cubic-bezier-curve-using-actionscript-3*/
+//fill Handle of the jug
 self.fillHandle = function(){
   var handLabel, x = 0, y = 0;
   var brewArr = ["The","Hackers","Brew"],brewIndex=0;
@@ -130,23 +142,27 @@ self.fillHandle = function(){
   while(brewIndex < 5){
     (function(controlX,controlY,anchorY1,controlY1,controlY2,anchorY2){
       for(var t=0;t<=1;t+=1/20){
+       //two control points for better curve manipulation
         x = Math.pow(t,3)*anchorX1+3*Math.pow(t,2)*(1-t)*controlX+3*Math.pow((1-t),2)*t*controlY+Math.pow((1-t),3)*anchorX2;
         y = Math.pow(t,3)*anchorY1+3*Math.pow(t,2)*(1-t)*controlY1+3*Math.pow((1-t),2)*t*controlY2+Math.pow((1-t),3)*anchorY2;
         console.log(">>>>x in bezier curve<<<<<"+x);
         console.log(">>>>y in bezier curve<<<<<"+y);
+       //to handle the merging of text elements for subsequent rows
+        if(brewIndex < 3) {x += 10;} 
         handLabel =  self.paper.text(x,y,brewArr[Math.floor(Math.random()*3)]);
         }
     })(controlX1,controlX2,anchorY1,controlY1,controlY2,anchorY2);
-    controlX1 += 10;
+//  controlX1 += 10;
     controlX2 += 10;
-    anchorY1 -= 5;
+    anchorY1 -= 10;
     anchorY2 -= 10;
-    controlY1 -= 40;
+    controlY1 -= 5;
     controlY2 -= 10;
     brewIndex++;
   }
 }
 
+//yet to be done :-)
 self.fillFoam = function(){
   
 }
@@ -161,6 +177,8 @@ var booze;
 
 //Color Sample => #EAB102
 
+//Array of objects to interate over to fetch text contents from
+//Need to update the array with more languages
 jQuery(function(){
   booze = new booze('booze',2000,800,[
     {ingredient:'C++',x:100,y:100,size:15},
